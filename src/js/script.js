7a15388c4c34b6dc142272f057db571dceda2cb6
Nikita Denisov <ndenisov@crocs.com>
2017-06-16 12:53:27 +0300

    Finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,21 @@
+
+
 function convert () {
+	let resultClass = document.getElementsByClassName("result")[0];
 	let tSessi = parseInt(document.getElementById("inputSessi").value);
 	let tFar = (9 / 5) * (tSessi + 32);
-	document.getElementsByClassName("result")[0].style.minWidth = "30%";
-	document.getElementsByClassName("result")[0].style.backgroundColor = "#94bb82";
-	document.getElementsByClassName("result")[0].innerHTML = "F "+ tFar;
+	if ( !(resultClass.classList.contains("result--exposed")) ) {
+		resultClass.classList.add("result--exposed");
+	}
+	document.getElementsByClassName("result__temp")[0].innerHTML = "F "+ tFar;
+}
+
+function putVar () {
+	let resultClass = document.getElementsByClassName("result")[1];
+	let name = document.getElementById("inputName").value;
+	let admin = name;
+	if ( !(resultClass.classList.contains("result--exposed")) ) {
+		resultClass.classList.add("result--exposed");
+	}
+	document.getElementsByClassName("result__admin")[0].innerHTML = admin;
 }
