56e8adee35d00b82bf157b9e2a6ede8feaa4011a
illmouse <nadenisov@gmail.com>
2017-06-21 19:35:29 +0300

    Complete main tasks.

--- src/hw2/js/script.js
+++ src/hw2/js/script.js
@@ -1,24 +1,91 @@
 
-function diffNum() {
 
-	let a = parseInt(document.getElementById("a3").value);
-	let b = parseInt(document.getElementById("b3").value);
-	let x;
 
-	if ((a + b) !== NaN && Number.isInteger(a + b)) {
-		if (a >= 0 && b >= 0) {
-			if (a >= b) {
-				x = a - b;
+
+function numInteger() {
+	let a3 = parseInt(document.getElementById("a3").value);
+	let b3 = parseInt(document.getElementById("b3").value);
+	let x3;
+
+	if ((a3 + b3) !== NaN && Number.isInteger(a3 + b3)) {
+		if (a3 >= 0 && b3 >= 0) {
+			if (a3 >= b3) {
+				x3 = a3 - b3;
 			} else {
-				x = b - a;
+				x3 = b3 - a3;
 			}
-		} else if ((a >= 0 && b < 0) || (a < 0 && b >= 0)) {
-				x = a + b;
-		} else if (a < 0 && b < 0) {
-			x = a * b;
+		} else if ((a3 >= 0 && b3 < 0) || (a3 < 0 && b3 >= 0)) {
+				x3 = a3 + b3;
+		} else if (a3 < 0 && b3 < 0) {
+			x3 = a3 * b3;
 		}
-		document.getElementsByClassName("result--3")[0].innerHTML = x;
+		document.getElementsByClassName("result--3")[0].innerHTML = x3;
 	} else {
 		document.getElementsByClassName("result--3")[0].innerHTML = "Wrong input!";
 	}
 }
+
+function varSwitch() {
+	let a4 = parseInt(document.getElementById("a4").value);
+	let result4 = document.getElementsByClassName("result--4")[0].innerHTML = ""
+	do {
+		result4 = document.getElementsByClassName("result--4")[0].innerHTML
+		switch (true) {
+			case a4 < 16:
+				document.getElementsByClassName("result--4")[0].innerHTML = result4 + a4 + " ";
+				++a4;
+				break;
+		}
+	} while (a4 < 16);
+}
+
+function numSumm(a5,b5) {
+	// var a5 = parseInt(document.getElementById("a5").value);
+	// var b5 = parseInt(document.getElementById("b5").value);
+	return a5 + b5;
+}
+function numDiff(a5,b5) {
+	// var a5 = parseInt(document.getElementById("a5").value);
+	// var b5 = parseInt(document.getElementById("b5").value);
+	return a5 - b5;
+}
+function numMult(a5,b5) {
+	// var a5 = parseInt(document.getElementById("a5").value);
+	// var b5 = parseInt(document.getElementById("b5").value);
+	return a5 * b5;
+}
+function numDiv(a5,b5) {
+	// var a5 = parseInt(document.getElementById("a5").value);
+	// var b5 = parseInt(document.getElementById("b5").value);
+	return a5 / b5;
+}
+
+function numMultiaction(a6, b6, op6) {
+	var a6 = parseInt(document.getElementById("a6").value);
+	var b6 = parseInt(document.getElementById("b6").value);
+	var op6 = document.getElementById("op6").value;
+	var x6;
+	switch (a6, b6, op6) {
+		case op6 = "Summ":
+			x6 = numSumm(a6,b6);
+			break;		
+			case op6 = "Diff":
+			x6 = numDiff(a6,b6);
+			break;		
+			case op6 = "Mult":
+			x6 = numMult(a6,b6);
+			break;		
+			case op6 = "Div":
+			x6 = numDiv(a6,b6);
+			break;
+	}
+	return x6;
+}
+
+function writeHTML(where, what) {
+	document.getElementsByClassName(where)[0].innerHTML = what;
+}
+
+// function numResult() {
+// 	document.getElementsByClassName("result--5")[0].innerHTML = x5;
+// }
