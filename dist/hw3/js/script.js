09f39c77bd1ec9201e841a69f4b12c139c31541b
illmouse <nadenisov@gmail.com>
2017-06-23 00:27:05 +0300

    HW3

---
+++ dist/hw3/js/script.js
@@ -0,0 +1,44 @@
+// Задание №1
+console.log("\t ***** Задание №1 *****");
+
+	var i = 2;
+	var n = 1;
+	while (i < 10) {
+		if (((i - 1) % n) != 0) {
+			++n;
+		} else {
+			console.log(i);
+			n = 1;
+			++i
+		}
+	}
+
+// Задание №2
+console.log("\t ***** Задание №2 *****");
+
+	var i = 0;
+
+	do {
+		if (i == 0) {
+			console.log(i + " – это ноль");
+		} else if ((i % 2) == 0) {
+			console.log(i + " – четное число");
+		} else {
+			console.log(i + " – нечетное число");
+		}
+		++i
+	} while (i <= 10)
+
+	
+
+// Задание №3
+console.log("\t ***** Задание №3 *****");
+
+	for (var i = 0; i < 9; console.log(++i)) {}
+
+// Задание №4
+console.log("\t ***** Задание №4 *****");
+
+	for (var i = 0, x = "x"; i < 20; i++) {
+		i = 0 ? console.log("x") : console.log(x = x + "x");
+	}
