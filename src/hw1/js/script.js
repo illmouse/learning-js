dd86b1878cdffcc35945282a6234d44b2e47557a
illmouse <nadenisov@gmail.com>
2017-07-22 16:24:16 +0300

    JS2 - HW1 (1 unit)

--- src/hw1/js/script.js
+++ src/hw1/js/script.js
@@ -1,21 +1,60 @@
+'use sctrict'
 
-
-function convert () {
-	let resultClass = document.getElementsByClassName("result")[0];
-	let tSessi = parseInt(document.getElementById("inputSessi").value);
-	let tFar = (9 / 5) * tSessi + 32;
-	if ( !(resultClass.classList.contains("result--exposed")) ) {
-		resultClass.classList.add("result--exposed");
-	}
-	document.getElementsByClassName("result__temp")[0].innerHTML = "F "+ tFar;
+function Container() {
+  this.id = "";
+  this.className= "";
+  this.htmlCode = "";
 }
 
-function putVar () {
-	let resultClass = document.getElementsByClassName("result")[1];
-	let name = document.getElementById("inputName").value;
-	let admin = name;
-	if ( !(resultClass.classList.contains("result--exposed")) ) {
-		resultClass.classList.add("result--exposed");
-	}
-	document.getElementsByClassName("result__admin")[0].innerHTML = admin;
+Container.prototype.render = function() {
+  return this.htmlCode;
 }
+
+Container.prototype.remove = function( elem ) {
+  elem = document.getElementsByClassName( this.className );
+  elem[0].remove();
+}
+
+function Menu( my_id, my_class, my_items ) {
+  Container.call( this );
+  this.id = my_id;
+  this.className = my_class;
+  this.items = my_items;
+}
+Menu.prototype = Object.create( Container.prototype );
+Menu.prototype.constructor = Menu;
+
+
+function MenuItem( itemHref, itemName ) {
+  Container.call( this );
+  this.className = "menu-item menu-item--" + itemIndex;
+  this.itemHref = itemHref;
+  this.itemName = itemName;
+  itemIndex++;
+}
+MenuItem.prototype = Object.create( Container.prototype );
+MenuItem.prototype.constructor = MenuItem;
+
+MenuItem.prototype.render = function() {
+  return this.htmlCode = `<li class="${this.className}" href="${this.itemHref}">${this.itemName}</li>`;
+}
+
+var itemIndex = 1;
+var m_item1 = new MenuItem( "/", "Главная" );
+var m_item2 = new MenuItem( "/catalogue/", "Каталог" );
+var m_item3 = new MenuItem( "/gallery/", "Галерея" );
+var m_items = { 0: m_item1, 1: m_item2, 2: m_item3 };
+
+Menu.prototype.render = function() {
+  var result = `<ul class="${this.className}" id="${this.id}">`;
+  for ( var item in this.items ) {
+    // if ( this.items[item] instanceof MenuItem ) {
+      result += this.items[item].render();
+    // }
+  }
+  result += `</ul>`;
+  return this.htmlCode = result;
+}
+
+var menu = new Menu( "menu", "menu", m_items );
+document.write( menu.render() );
